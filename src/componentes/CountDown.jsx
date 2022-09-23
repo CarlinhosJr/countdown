e017409a720f50e2620f90cd013b5625f8c83944
graziosi <luizgraziosi@hotmail.com>
2022-09-23 11:39:35 -0300

    update sintaxe

--- src/componentes/CountDown.jsx
+++ src/componentes/CountDown.jsx
@@ -4,7 +4,7 @@ import dayjs from 'dayjs';
 const CountDown = () => {
 
     const nowDate = new Date()
-    const date = new Date('2022-09-22 00:00:00');
+    const date = new Date('2022-09-23 09:09:00');
     const falta = (date.getTime() - new Date().getTime()) / 1000;
 
     const [seconds, setSeconds] = useState(Math.round(falta % 60));
@@ -15,7 +15,7 @@ const CountDown = () => {
     useEffect(() =>{
         const intervalId = setInterval(() => {
             if(seconds == 0){
-                setSeconds (sec => sec = 60)
+                setSeconds (60)
                 setMinutes(minutes -1)
             }
             if(minutes == 0){
@@ -36,10 +36,10 @@ const CountDown = () => {
     }, [seconds, minutes, hours, days])
 
     const isTimeBefore = () => {
-        setDays(day => day = 0)
-        setHours(hour => hour = 0)
-        setMinutes(minute => minute = 0)
-        setSeconds(second => second = 0)
+        setDays(0)
+        setHours(0)
+        setMinutes(0)
+        setSeconds(0)
     }
 
 
